3e687070569317284c0e8d80adb1b3256e9797de
hugo12345678999 <vendpromax@gmail.com>
2024-07-20 14:49:29 -0300

    first commit

--- src/components/Main/SearchPagamentos/EditPagamento.js
+++ src/components/Main/SearchPagamentos/EditPagamento.js
@@ -21,6 +21,8 @@ const EditPagamento = (props) => {
     nome: maquinaInfos?.nome ?? "",
     descricao: maquinaInfos?.descricao ?? "",
     estoque: Number(maquinaInfos?.estoque) ?? 0,
+    contadorcredito: Number(maquinaInfos?.contadorcredito) ?? 0,
+    contadorpelucia: Number(maquinaInfos?.contadorpelucia) ?? 0,
     store_id: Number(maquinaInfos?.storeId) ?? 0,
     valorDoPulso: maquinaInfos?.pulso ?? 0,
   });
@@ -74,6 +76,8 @@ const EditPagamento = (props) => {
           nome: data.nome,
           descricao: data.descricao,
           estoque: Number(data.estoque),
+          contadorcredito: Number(data.contadorcredito),
+          contadorpelucia: Number(data.contadorpelucia),
           store_id: String(data.store_id),
           valorDoPulso: data.valorDoPulso,
         },
